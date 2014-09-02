8144f38d829ad1c53f9d84f6620cb9e9c9a2b85f
RobotOptimist <james.macivor1@gmail.com>
2014-09-02 07:50:05 -0400

    linked to Sunlight API
    linked to Sunlight API in order to look up congress person name by
    zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,31 @@
+require "csv"
+require "sunlight/congress"
 puts "Event Manager Initialized!"
 
+Sunlight::Congress.api_key = "8c7a9be6703a42f296f40a3aebeeba5f"
+
+def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+
+  legislator_names.join(", ")
+end
+
+
 Dir.chdir("..")
-lines = File.readlines("event_attendees.csv")
-lines.each_with_index do |line,index| 
-	next if index == 0
-	columns = line.split(',')
-	names = columns[2]
-	puts names
+contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
+contents.each do |row| 
+	name = row[:first_name]
+	zipcode = clean_zipcode(row[:zipcode])
+	
+	legislators_string = legislators_by_zipcode(zipcode)
+	
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
