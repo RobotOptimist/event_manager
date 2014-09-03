0b5608d95946f449568f63230fa3ecc2d528e200
RobotOptimist <james.macivor1@gmail.com>
2014-09-03 08:08:40 -0400

    erb refresher, phone number clean
    started phone number clean up problem, finished erb tutorial

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,7 @@
 require "csv"
 require "sunlight/congress"
+require 'erb'
+
 puts "Event Manager Initialized!"
 
 Sunlight::Congress.api_key = "8c7a9be6703a42f296f40a3aebeeba5f"
@@ -9,23 +11,47 @@ def clean_zipcode(zipcode)
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislator_names.join(", ")
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
 
-Dir.chdir("..")
-contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
-contents.each do |row| 
-	name = row[:first_name]
-	zipcode = clean_zipcode(row[:zipcode])
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
+file_path = File.join('C:','users', 'macivor', 'desktop', 'the odin project', 'event_manager', 'event_attendees.csv')
+contents = CSV.open(file_path, headers: true, header_converters: :symbol)
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
+#contents.each do |row|
+	#id = row[0]
+  #name = row[:first_name]
+
+  #zipcode = clean_zipcode(row[:zipcode])
+
+  #legislators = legislators_by_zipcode(zipcode)
+
+  #form_letter = erb_template.result(binding)
 	
-	legislators_string = legislators_by_zipcode(zipcode)
-	
-  puts "#{name} #{zipcode} #{legislators_string}"
+	#save_thank_you_letters(id,form_letter)
+#end
+
+contents.each do |row|
+	phone_number = row[:homephone]
+	phone_number.gsub!(/\D/,'')
+	if phone_number.length < 10
+		phone_number = phone_number.ljust(10,"0")
+	end
+	if phone_number.length == 11 && phone_number.start_with?("1")
+		phone_number = phone_number[1,10]
+	end
+	phone_number = phone_number[0,3] + "-" + phone_number[4,4] + "-" + phone_number[6,9]
+	puts phone_number
 end
